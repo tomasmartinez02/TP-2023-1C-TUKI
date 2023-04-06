34b97695c5cb0f9ae4060f6769331d553c449bbe
tomasmartinez02 <tomamartinez@frba.utn.edu.ar>
2023-04-06 10:08:31 -0300

    Configuracion .h

---
+++ kernel/src/kernel.h
@@ -0,0 +1,16 @@
+#define CONSOLA_H_
+
+// Bibliotecas estandar
+#include <stdlib.h>
+#include <stdio.h>
+// Bibliotecas commons
+// Biblioteca internas
+// Bibliotecas static utils
+
+// Estructuras (TADS)
+
+
+// Prototipos
+
+// Variables globales
+#endif
