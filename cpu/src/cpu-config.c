146b0ce104a200a7bd23472c11857757d74748a2
irinaperezg <iprezgribnicow@frba.utn.edu.ar>
2023-04-11 23:30:41 +0000

    errores arreglados

--- cpu/src/cpu-config.c
+++ cpu/src/cpu-config.c
@@ -9,8 +9,8 @@ static void __cpu_config_initializer(void *moduleConfig, t_config *tempCfg)
 
     cpuConfig->RETARDO_INSTRUCCION = config_get_int_value(tempCfg, "RETARDO_INSTRUCCION");
     cpuConfig->IP_MEMORIA = strdup(config_get_string_value(tempCfg, "IP_MEMORIA"));
-    cpuConfig->PUERTO_MEMORIA = config_get_int_value(tempCfg, "PUERTO_MEMORIA");
-    cpuConfig->PUERTO_ESCUCHA = config_get_int_value(tempCfg, "PUERTO_ESCUCHA");
+    cpuConfig->PUERTO_MEMORIA = strdup(config_get_string_value(tempCfg, "PUERTO_MEMORIA"));
+    cpuConfig->PUERTO_ESCUCHA = strdup(config_get_string_value(tempCfg, "PUERTO_ESCUCHA"));
     cpuConfig->TAM_MAX_SEGMENTO = config_get_int_value(tempCfg, "TAM_MAX_SEGMENTO");
 
     return;
@@ -24,7 +24,6 @@ t_cpu_config *cpu_config_create(char *cpuConfigPath, t_log *cpuLogger)
     int inicializacionCorrecta = config_init(self, cpuConfigPath, cpuLogger, __cpu_config_initializer);
     if (inicializacionCorrecta == -1) {
         exit(EXIT_FAILURE);
-        //no habria q loggear algo aca? idk
     }
 
     return self;
@@ -34,12 +33,14 @@ t_cpu_config *cpu_config_create(char *cpuConfigPath, t_log *cpuLogger)
 void consola_config_destroy(t_cpu_config *self)
 {   
     free(self->IP_MEMORIA);
+    free(self->PUERTO_ESCUCHA);
+    free(self->PUERTO_MEMORIA);
     free(self);
 
     return;
 }
 
-int32_t cpu_config_get_retardo_instruccion(t_cpu_config *self)
+uint32_t cpu_config_get_retardo_instruccion(t_cpu_config *self)
 {
     return self->RETARDO_INSTRUCCION;
 }
@@ -49,17 +50,17 @@ char* cpu_config_get_ip_memoria(t_cpu_config *self)
     return self->IP_MEMORIA;
 }
 
-int32_t cpu_config_get_puerto_escucha(t_cpu_config *self)
+char* cpu_config_get_puerto_escucha(t_cpu_config *self)
 {
     return self->PUERTO_ESCUCHA;
 }
 
-int32_t cpu_config_get_puerto_memoria(t_cpu_config *self)
+char* cpu_config_get_puerto_memoria(t_cpu_config *self)
 {
     return self->PUERTO_MEMORIA;
 }
 
-int32_t cpu_config_get_tam_max_segmento(t_cpu_config *self)
+uint32_t cpu_config_get_tam_max_segmento(t_cpu_config *self)
 {
     return self->TAM_MAX_SEGMENTO;
 }
