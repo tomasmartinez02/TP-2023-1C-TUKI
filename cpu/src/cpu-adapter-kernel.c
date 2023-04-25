4a9d44d27609754af00321940a2ab04343a191e6
irinaperezg <iprezgribnicow@frba.utn.edu.ar>
2023-04-25 20:51:20 -0300

    correcciones lista de instrucciones

--- cpu/src/cpu-adapter-kernel.c
+++ cpu/src/cpu-adapter-kernel.c
@@ -51,12 +51,14 @@ t_cpu_pcb* __desempaquetar_pcb(t_buffer* bufferPcb, t_buffer* bufferInstruccione
 {   
     uint32_t pid, programCounter;
     t_registros_cpu *registrosCpu;
+    t_list *instrucciones;
 
     buffer_unpack(bufferPcb, &pid, sizeof(pid));
     buffer_unpack(bufferPcb, &programCounter, sizeof(programCounter));
     registrosCpu = __desempaquetar_registros_de_buffer(bufferPcb);
-    
-    t_cpu_pcb* pcb = crear_pcb(pid, programCounter, registrosCpu, bufferInstrucciones);
+    instrucciones = instruccion_list_create_from_buffer(bufferInstrucciones, cpuLogger);
+
+    t_cpu_pcb* pcb = crear_pcb(pid, programCounter, registrosCpu, instrucciones);
     return pcb;
 }
 
@@ -64,7 +66,11 @@ t_cpu_pcb* __desempaquetar_pcb(t_buffer* bufferPcb, t_buffer* bufferInstruccione
 t_cpu_pcb* recibir_pcb_de_kernel() 
 {      
     int socketKernel = cpu_config_get_socket_kernel(cpuConfig);
-    t_buffer *bufferPcb, *bufferInstrucciones;
+    t_buffer *bufferPcb = NULL;
+    t_buffer *bufferInstrucciones = NULL;
+
+    bufferPcb = buffer_create();
+    bufferInstrucciones = buffer_create();
 
     t_header headerPcb = stream_recv_header(socketKernel);
     stream_recv_buffer(socketKernel, bufferPcb);
@@ -79,6 +85,10 @@ t_cpu_pcb* recibir_pcb_de_kernel()
     }
 
     t_cpu_pcb* pcb = __desempaquetar_pcb(bufferPcb, bufferInstrucciones);
+
+    free(bufferPcb);
+    free(bufferInstrucciones);
+
     return pcb;
 }
 
@@ -102,7 +112,7 @@ void cpu_ejecutar_instruccion(t_instruccion *instruccion, t_cpu_pcb *pcb)
 {
     // tampoco es void, después lo cambio
 
-    t_tipo_instruccion *tipoInstruccion = instruccion_get_tipo_instruccion(instruccion);
+    t_tipo_instruccion tipoInstruccion = instruccion_get_tipo_instruccion(instruccion);
 
     switch (tipoInstruccion)
     {
@@ -116,52 +126,34 @@ void cpu_ejecutar_instruccion(t_instruccion *instruccion, t_cpu_pcb *pcb)
 
         intervalo_de_pausa(retardoInstruccion);
         
-        // hacer el set del valor en el registro
+        // hacer el set del valor en el registro [no existe t_Registro_to_Char]
 
-        log_info(cpuLogger, "PID: <%d> - Ejecutando: <SET> - <%s> - <%d>", cpu_pcb_get_pid(pcb), t_registro_to_char(registro), valor);
-        log_info(cpuDebuggingLogger, "PID: <%d> - Ejecutando: <SET> - <%s> - <%d>", cpu_pcb_get_pid(pcb), t_registro_to_char(registro), valor);
+        //log_info(cpuLogger, "PID: <%d> - Ejecutando: <SET> - <%s> - <%d>", cpu_pcb_get_pid(pcb), t_registro_to_char(registro), valor);
+        //log_info(cpuDebuggingLogger, "PID: <%d> - Ejecutando: <SET> - <%s> - <%d>", cpu_pcb_get_pid(pcb), t_registro_to_char(registro), valor);
 
-        cpu_pcb_set_program_counter(pcb, incrementar_program_counter(pcb));
+        incrementar_program_counter(pcb);
 
         break;
     case INSTRUCCION_yield:
+        log_info(cpuLogger, "PID: <%d> - Ejecutando: <YIELD>", cpu_pcb_get_pid(pcb));
+        log_info(cpuDebuggingLogger, "PID: <%d> - Ejecutando: <YIELD>", cpu_pcb_get_pid(pcb));
 
+        t_buffer *bufferYield = buffer_create();
+        buffer_destroy(bufferYield);
         break;
+
     case INSTRUCCION_exit:
 
         log_info(cpuLogger, "PID: <%d> - Ejecutando: <EXIT>", cpu_pcb_get_pid(pcb));
         log_info(cpuDebuggingLogger, "PID: <%d> - Ejecutando: <EXIT>", cpu_pcb_get_pid(pcb));
 
         t_buffer *bufferExit = buffer_create();
-
-        // empaquetar pid
-        uint32_t pid = cpu_pcb_get_pid(pcb);
-        buffer_pack(bufferExit, &pid, sizeof(pid));
-
-        // empaquetar registros
-        t_registros_cpu *registrosActuales = cpu_pcb_get_registros(pcb);
-        // falta empaquetarlos
-
-        // empaquetar program counter
-        uint32_t programCounterActual = incrementar_program_counter(pcb);
-        buffer_pack(bufferExit, &programCounterActual, sizeof(programCounterActual));
-
-        stream_send_buffer(cpu_config_get_socket_kernel(cpuConfig), HEADER_proceso_terminado, bufferExit);
         buffer_destroy(bufferExit);
 
         break;
     default:
         break;
     }
-}
-
-void ejecutar_programa(t_cpu_pcb* pcb)
-{
-    int programCounter = cpu_pcb_get_program_counter(pcb);
-    //instrucciones = cpu_get_instrucciones(pcb);
-    // Función a hacer a futuro que busque en el buffer de instrucciones la instruccion que corresponda y la ejecute
-    //ejecutar_instruccion(instrucciones, programCounter);
-    incrementar_program_counter(pcb);
 }
 
 void incrementar_program_counter(t_cpu_pcb* pcb)
