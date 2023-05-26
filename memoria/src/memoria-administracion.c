36a3d9c4e81970bfdb38a8995ea6314691f9091f
tomasmartinez02 <tomamartinez@frba.utn.edu.ar>
2023-05-26 20:32:13 -0300

    Primeras estructuras

---
+++ memoria/src/memoria-administracion.c
@@ -0,0 +1,31 @@
+#include <memoria-config.h>
+
+// Variables estaticas
+static void* memoriaPrincipal; // por el momento va aca, despues vemos donde va
+
+// Funciones privadas
+
+// Funciones inicializacion estructuras de la memoria
+
+static void __inicializar_memoria_principal(void){
+    
+    uint32_t tamanioMemoria;
+    tamanioMemoria = memoria_config_get_tamanio_memoria(memoriaConfig);
+
+    memoriaPrincipal = malloc(tamanioMemoria); // Esto nunca se libera no?
+
+    log_info(memoriaDebuggingLogger, "Se inicializa a la memoria principal con un tamanio de %d bytes", tamanioMemoria);
+
+    return;
+
+}
+
+// Funciones publicas
+
+void inicializar_memoria(void){
+    
+    __inicializar_memoria_principal();
+
+    return;
+
+}
---
+++ memoria/src/memoria-administracion.h
@@ -0,0 +1,15 @@
+// Biblioteca para guardar las estructuras y variables globales del kernel
+#ifndef MEMORIA_ADMINISTRACION_H_
+#define MEMORIA_ADMINISTRACION_H_
+
+// Bibliotecas commons
+#include <commons/log.h>
+
+// Bibliotecas internas
+#include <memoria-utils.h>
+
+// Prototipos
+
+void inicializar_memoria(void);
+
+#endif
