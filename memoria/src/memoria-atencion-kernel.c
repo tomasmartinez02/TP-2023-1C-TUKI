60bccde791d75bb08bbe3adf002ec0079253b0d4
martudominguez <martdominguez@frba.utn.edu.ar>
2023-06-07 14:45:22 -0300

    avances memoria
    Co-authored-by: tomasmartinez02 <tomasmartinez02@users.noreply.github.com>

---
+++ memoria/src/memoria-atencion-kernel.c
@@ -0,0 +1,38 @@
+#include <memoria-atencion-kernel.h>
+
+// Funciones privadas
+
+// Funciones publicas
+
+void atender_peticiones_kernel(void)
+{
+
+    for (;;) {
+        int socketKernel = memoria_config_get_socket_kernel(memoria_config);
+        t_header headerRecibido = stream_recv_header(socketKernel);
+        t_buffer* bufferRecibido = buffer_create();
+        
+        switch (headerRecibido) {
+            case HEADER_crear_segmento:
+            {
+                break;
+            }
+            case HEADER_eliminar_segmento: // Este header hay q agregarlo
+            {
+
+            }
+            case HEADER_compactar: // Este header hay q agregarlo
+            {
+
+            }
+            default:
+            {   
+                // ERROR
+                break;
+            }
+        }
+        buffer_destroy(bufferRecibido);
+    }
+
+    return;
+}
---
+++ memoria/src/memoria-atencion-kernel.h
@@ -0,0 +1,17 @@
+#ifndef MEMORIA_ATENCION_KERNEL_H_
+#define MEMORIA_ATENCION_KERNEL_H_
+
+// Bibliotecas estandar
+// Bibliotecas static utils
+#include <serializacion/stream.h>
+#include <utils/common-utils.h>
+#include <serializacion/buffer.h>
+// Bibliotecas commons
+// Bibliotecas internas modulo cpu
+#include <memoria-config.h>
+
+// Prototipos
+
+void atender_peticiones_kernel(void);
+
+#endif
